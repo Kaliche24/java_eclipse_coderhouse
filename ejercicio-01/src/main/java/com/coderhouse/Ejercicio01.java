6e5d198e608ed02e4d093b8380e9f87015b06dcf
Subrogados <Subrogados@192.168.1.159>
2024-11-15 14:01:37 -0600

    Ejercicio 01 Done

---
+++ ejercicio-01/src/main/java/com/coderhouse/Ejercicio01.java
@@ -0,0 +1,18 @@
+package com.coderhouse;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leer = new Scanner(System.in);
+		int edad = 0;
+		
+		System.out.println("Ingresa la edad: ");
+		edad = leer.nextInt();
+		
+		System.out.println("Tu edad es: " + edad);
+	}
+
+}
